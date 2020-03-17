7b3181c85a6c88b3a1f3be1ac01c4d32bb37af16
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-17 14:17:29 +0200

    init

--- SwiftUIInPracticeUIAlertController/ContentView.swift
+++ SwiftUIInPracticeUIAlertController/ContentView.swift
@@ -9,8 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: {
+                self.presentTextFieldAlertController()
+            }) {
+                Text("Show alert")
+            }
+            Divider()
+            Text("Text: \(text)")
+        }
+    }
+    
+    private func presentTextFieldAlertController() {
+        if let rootViewController = UIApplication.shared.windows[0].rootViewController {
+            rootViewController.present(textFieldAlertController(), animated: true, completion: nil)
+        }
+    }
+    
+    private func textFieldAlertController() -> UIAlertController {
+        let textFieldAlertController = UIAlertController(title: "Save text", message: nil, preferredStyle: .alert)
+        
+        textFieldAlertController.addTextField()
+        
+        let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel"), style: .cancel, handler: nil)
+        
+        let saveAction = UIAlertAction(title: NSLocalizedString("Save", comment: "Save"), style: .default) { (alertAction) in
+            
+            self.text = textFieldAlertController.textFields![0].text ?? ""
+        }
+        
+        textFieldAlertController.addAction(cancelAction)
+        textFieldAlertController.addAction(saveAction)
+        
+        return textFieldAlertController
     }
 }
 
